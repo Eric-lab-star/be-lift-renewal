ba243a8965d4e32387b981e0cafdef6697388bb4
kimkyungsub <cyon256@icloud.com>
2024-08-24 19:51:47 +0900

    transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 "use client"
-import { useSpring, animated } from "@react-spring/web"
-import { useState } from "react"
+import { useSpring, animated, useTransition } from "@react-spring/web"
+import { useState} from "react"
 
 
 export default function Home() {
 	const [styles, api] = useSpring(()=>({x:0}))
 	const [toggle, setToggle] = useState(false)
+	const transition = useTransition(toggle, {
+		from: {
+			x: 0,
+			opacity: 0
+		},
+		enter:{
+			x: 100,
+			opacity: 1
+			
+		},
+		leave:{
+			x:0,
+			opacity: 0
+		}
+	})
+
 	function handleClick() {
 		if(!toggle){
 			api.start({x: 100})
@@ -18,16 +34,18 @@ export default function Home() {
 
 
   return (
-		<animated.div
-			onClick={handleClick}
-		  style={{
-			width: 80,
-			height: 80,
-			borderRadius: 8,
-			backgroundColor: "blue",
-			...styles
-		  }}
-		/>
+	<div className="container border-amber-500 border-2 border-dashed mx-auto">
+		<button className="rounded-md bg-amber-500 p-2" onClick={()=>handleClick()}>click me!</button>
+		{
+			transition((transitionStyle, toggle)=>(
+				<>
+				{
+					toggle ? <animated.div className="w-20 h-20 bg-amber-500" style={transitionStyle}></animated.div> : null
+				}
+				</>
+			))
+		}
+	</div>
 	)
 }
 
