6d4ca4a0012d78b23fc4825e257f6c47cd2e663a
kimkyungsub <cyon256@icloud.com>
2024-08-24 22:25:12 +0900

    anmation

--- src/app/UI/sideBars/largeScreenSideBar.tsx
+++ src/app/UI/sideBars/largeScreenSideBar.tsx
@@ -5,17 +5,37 @@ import Link from "next/link"
 import clsx from "clsx"
 import { darkText } from "@/app/styles"
 import Footer from "../footer"
+import {animated, useTransition } from "@react-spring/web"
 
 export default function LargeScreenSideBar(){
 	const sideBarState = useContext(SideBarCtx)
-	if(sideBarState){
-		return <OpenState /> 
-	}else {
-		return <ClosedState />
+	const transition = useTransition(sideBarState, {
+		from:{
+			transform: "translateX(100%)",
+			opactiy: 0,
+		},
+		enter:{
+			transform: "translateX(0)",
+			opactiy: 1,
+		},
+		leave:{
+			transform: "translateX(-50%)",
+			opactiy: 0,
+		},
+
+	})
+	return <div>
+	{
+		transition((style, sideBarState) =>(
+			<>{sideBarState ? <animated.div style={style}><OpenState/></animated.div>: <animated.div style={style}><ClosedState/></animated.div>}</>
+		))
 	}
+	</div>
+
 }
 
 function OpenState(){
+
 	return <div id="Links" className={ clsx(`dark:bg-amber-700/30 text-sm dark:${darkText} bg-white/80 flex flex-col border-b-gray-300 dark:border-amber-600/10 border w-48 pt-10  h-screen`, )} >
 		<div className="flex-1">
 			{links.map((link)=>{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
 "use client"
-import { useSpring, animated, useTransition } from "@react-spring/web"
-import { useState} from "react"
+import { useSpring, animated, useTransition, AnimatedProps } from "@react-spring/web"
+import { CSSProperties, useState} from "react"
 
 
 export default function Home() {
 	const [styles, api] = useSpring(()=>({x:0}))
 	const [toggle, setToggle] = useState(false)
 	const transition = useTransition(toggle, {
+		keys: null,
 		from: {
-			x: 0,
-			opacity: 0
+			transform: "translate3d(100%, 0, 0)",
+			opacity: 0,
 		},
 		enter:{
-			x: 100,
-			opacity: 1
+			transform: "translate3d(0%, 0, 0)",
+			opacity: 1,
 			
 		},
 		leave:{
-			x:0,
-			opacity: 0
+			transform: "translate3d(-100%, 0, 0)",
+			opacity: 0,
 		}
 	})
 
@@ -32,20 +33,27 @@ export default function Home() {
 		}
 	}
 
-
   return (
 	<div className="container border-amber-500 border-2 border-dashed mx-auto">
 		<button className="rounded-md bg-amber-500 p-2" onClick={()=>handleClick()}>click me!</button>
 		{
-			transition((transitionStyle, toggle)=>(
-				<>
-				{
-					toggle ? <animated.div className="w-20 h-20 bg-amber-500" style={transitionStyle}></animated.div> : null
-				}
-				</>
-			))
+			transition((transitionStyle, toggle)=>{
+				return <div className="relative">{toggle ? <BoxA style={transitionStyle}/> : <BoxB style={transitionStyle} />}</div>
+				
+			})
 		}
 	</div>
 	)
 }
 
+
+
+function BoxA({style}: AnimatedProps<{style: CSSProperties}>){
+return <animated.div className="bg-amber-500 w-40 h-40 text-lg font-semibold" style={style}>A</animated.div>
+}
+function BoxB({style}: AnimatedProps<{style: CSSProperties}>){
+	return <animated.div className="bg-amber-500 w-40 h-40 text-lg font-semibold" style={style}>B</animated.div>
+}
+function BoxC({style}: {style: CSSProperties}){
+	return <animated.div className="bg-amber-500 w-40 h-40 text-lg font-semibold" style={style}>C</animated.div>
+}
